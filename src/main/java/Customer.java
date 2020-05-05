56df58739c2cdaf9fc6ef6be645d5bcd0835694f
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-05-05 03:09:54 -0500

    local PMD fix commit

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -52,8 +52,8 @@ public class Customer {
             frequentRenterPoints++;
             
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                (each.getDaysRented() > 1)) {
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE &&
+                each.getDaysRented() > 1) {
                     frequentRenterPoints++;
             }
             
