ca6c7969ae7bac7a9594090f96c2d6f5d3f1f1a5
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-05-16 17:02:01 -0500

    move amountFor from Customer to Rental

--- src/main/java/me/sibalnirbhay/classes/Customer.java
+++ src/main/java/me/sibalnirbhay/classes/Customer.java
@@ -25,13 +25,13 @@ public class Customer {
         String result = header(name);
 
         for (Rental each : myRentals) {
-            double thisAmount = amountFor(each);
+            double thisAmount = each.amountFor();
 
             // show figures for this rental
             result      += "\t" + each.getMovie().getTitle() +
                            "\t" + String.valueOf(thisAmount) + "\n";
         }
-        
+
         double totalAmount = totalAmount(this.myRentals);
 
         int frequentRenterPoints = frequentRenterPoints(this.myRentals);
@@ -43,37 +43,10 @@ public class Customer {
     private double totalAmount(Iterable<Rental> rentals) {
         double totalAmount = 0;
         for (Rental each : rentals) {
-            double thisAmount = amountFor(each);
+            double thisAmount = each.amountFor();
             totalAmount += thisAmount;
         }
         return totalAmount;
-    }
-
-    private double amountFor(Rental rental) {
-        double thisAmount = 0;
-
-        // determine amounts for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-            default:
-                System.out.println("Bad Rental Type");
-                break;
-        }
-        return thisAmount;
     }
 
     private int frequentRenterPoints(List<Rental> rentals) {
